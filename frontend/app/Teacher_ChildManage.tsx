13cfef5a0d7dbaa2249d326f0c5f6fd94a80edbd
종효련 <jhl2_014@naver.com>
2026-02-06 13:59:43 +0900

    [S14P11E204-254] eas 빌드용 파일 잡기

--- frontend/app/Teacher_ChildManage.tsx
+++
@@ -1,5 +0,0 @@
-// 라우트용 파일
-
-import TeacherChildManagePage from './screens/Teacher_ChildManage'
-
-export default TeacherChildManagePage
--- frontend/app/services/lessonService.ts
+++
@@ -1,7 +0,0 @@
-import api from '../api/api';
-
-export const startLessonApi = async (classId: number) => {
-  // POST /classes/{classId}/session/start
-  const response = await api.post(`/classes/${classId}/session/start`);
-  return response.data; 
-};
